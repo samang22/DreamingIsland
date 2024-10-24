8591c237d8ee087b05f97b91a1a7983164801d38
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-10-24 10:31:46 +0900

    Moriblin SwordAttack done

---
+++ DreamingIsland/Source/DreamingIsland/Animation/AnimNotify/Monsters/AnimNotify_MoriblinSwordAttack.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/AnimNotify/Monsters/AnimNotify_MoriblinSwordAttack.h"
+#include "Misc/Utils.h"
+#include "Actors/Projectile/Projectile.h"
+#include "Actors/Projectile/ProjectileTableRow.h"
+#include "Actors/Monster.h"
+
+
+UAnimNotify_MoriblinSwordAttack::UAnimNotify_MoriblinSwordAttack()
+{
+}
+
+void UAnimNotify_MoriblinSwordAttack::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+#if WITH_EDITOR
+	if (GIsEditor && MeshComp->GetWorld() != GWorld) { return; } // 에디터 프리뷰
+#endif
+
+	AMonster* Monster = Cast<AMonster>(MeshComp->GetOwner());
+	check(Monster);
+
+	const FVector MonsterForwardVector = Monster->GetActorForwardVector();
+
+	UWorld* World = Monster->GetWorld();
+
+	AProjectile* Projectile = World->SpawnActorDeferred<AProjectile>(AProjectile::StaticClass(),
+		FTransform::Identity, Monster, Monster, ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+
+	FTransform NewTransform;
+	Projectile->SetData(ProjectileName::Albatoss_ClawAttack, CollisionProfileName::Monster_Projectile, COLLISION_CHANNEL_MONSTERPROJECTILE);
+	NewTransform.SetLocation(Monster->GetActorLocation() + MORIBLIN_SWORDATTACK_LENGTH * MonsterForwardVector);
+	NewTransform.SetRotation(FRotator::ZeroRotator.Quaternion());
+	Projectile->FinishSpawning(NewTransform);
+}
---
+++ DreamingIsland/Source/DreamingIsland/Animation/AnimNotify/Monsters/AnimNotify_MoriblinSwordAttack.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_MoriblinSwordAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DREAMINGISLAND_API UAnimNotify_MoriblinSwordAttack : public UAnimNotify
+{
+	GENERATED_BODY()
+public:
+	UAnimNotify_MoriblinSwordAttack();
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+protected:
+	UPROPERTY(EditAnywhere, meta = (RowType = "ProjectileTableRow"))
+	FDataTableRowHandle ProjectileTableRowHandle;
+};
