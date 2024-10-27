f331aefad9f85c3cdab5148b088ccce70cebea68
samang22 <minhyuk5757@gmail.com>
2024-10-27 21:43:55 +0900

    1. some static meshes's simple collsion erased 2. House actor added

---
+++ DreamingIsland/Source/DreamingIsland/Nav/NavArea_ExceptObstacle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NavAreas/NavArea.h"
+#include "NavArea_ExceptObstacle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DREAMINGISLAND_API UNavArea_ExceptObstacle : public UNavArea
+{
+	GENERATED_BODY()
+	
+};
