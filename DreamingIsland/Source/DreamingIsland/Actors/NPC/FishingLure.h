ed2fbd099a9eee54427def2ec6b8c6c5f68d90ae
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-11-15 09:25:28 +0900

    FishingLure overlap func added and binded

--- DreamingIsland/Source/DreamingIsland/Actors/NPC/FishingLure.h
+++ DreamingIsland/Source/DreamingIsland/Actors/NPC/FishingLure.h
@@ -52,6 +52,7 @@ protected:
 	TObjectPtr<AFish> CurrentFish = nullptr;
 
 protected:
+	UFUNCTION()
 	virtual void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	UFUNCTION()
 	virtual void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
