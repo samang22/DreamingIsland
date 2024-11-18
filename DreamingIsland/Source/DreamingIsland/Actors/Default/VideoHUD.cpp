880d7aecc7f57fb5cb9415420f450f9c1baadec6
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-11-18 10:34:43 +0900

    Playing video structure modifed

--- DreamingIsland/Source/DreamingIsland/Actors/Default/VideoHUD.cpp
+++ DreamingIsland/Source/DreamingIsland/Actors/Default/VideoHUD.cpp
@@ -24,37 +24,23 @@ void AVideoHUD::BeginPlay()
 	Super::BeginPlay();
 	UClass* WidgetClass = nullptr;
 
-	if (bIsOpening)
+	if (MediaPlayer && OpeningMediaSource)
 	{
-		if (MediaPlayer && OpeningMediaSource)
-		{
-			WidgetClass = LoadClass<UUserWidget>(nullptr, TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/UI/UI_Opening.UI_Opening_C'"));
-			OpeningWidget = CreateWidget<UOpeningWidget>(GetWorld(), WidgetClass);
-			OpeningWidget->AddToViewport();
+		WidgetClass = LoadClass<UUserWidget>(nullptr, TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/UI/UI_Opening.UI_Opening_C'"));
+		OpeningWidget = CreateWidget<UOpeningWidget>(GetWorld(), WidgetClass);
+		OpeningWidget->AddToViewport();
 
-			MediaPlayer->OpenSource(OpeningMediaSource);  
+		if (bIsOpening)
+		{
+			MediaPlayer->OpenSource(OpeningMediaSource);
 		}
-	}
-	else if (bIsTitle)
-	{
-		if (MediaPlayer && TitleMediaSource)
+		else if (bIsEnding)
 		{
-			WidgetClass = LoadClass<UUserWidget>(nullptr, TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/UI/UI_Title.UI_Title_C'"));
-			TitleWidget = CreateWidget<UTitleWidget>(GetWorld(), WidgetClass);
-			TitleWidget->AddToViewport();
-
-			MediaPlayer->OpenSource(TitleMediaSource);
-		}
-	}
-	else if (bIsEnding)
-	{
-		if (MediaPlayer && EndingMediaSource)
-		{
-			WidgetClass = LoadClass<UUserWidget>(nullptr, TEXT("/Script/UMGEditor.WidgetBlueprint'/Game/UI/UI_Ending.UI_Ending_C'"));
-			EndingWidget = CreateWidget<UEndingWidget>(GetWorld(), WidgetClass);
-			EndingWidget->AddToViewport();
-
 			MediaPlayer->OpenSource(EndingMediaSource);
+		}
+		else if (bIsTitle)
+		{
+			MediaPlayer->OpenSource(TitleMediaSource);
 		}
 	}
 }
